a121c7e446f25c30e9cf1c82e7829d86b74b323a
ychuang <ychuang@eland.com.tw>
2021-07-26 11:28:52 +0800

    Rename the CLI app filename...

--- click_app.py
+++ click_app.py
@@ -10,11 +10,14 @@ hf_logging.set_verbosity_error()
 @click.command()
 @click.option('--sentence', prompt='your sentence', help='This is a string that indicate what you want to classify')
 def prediction(sentence):
+    click.echo('\n')
+    click.echo('calculating scores...please wait')
+    click.echo('\n')
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = ADClassifier(len(['0', '1'])).to(device)
     score, label = single_prediction(model, sentence)
     if label == 0:
-        click.echo('This content is NOT an AD article, the probability is {0}'.format(score))
+        click.echo('This content is "NOT" an AD article, the probability is {0}'.format(score))
     else:
         click.echo('This content is an AD article, the probability is {0}'.format(score))
 
