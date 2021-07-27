ed91bc0e9cc4985846dee1c58b3f27ed1ffb81d9
ychuang <ychuang@eland.com.tw>
2021-07-27 13:25:22 +0800

    created new branch and web_api file

--- test_api.py
+++ test_api.py
@@ -1,3 +1,4 @@
+import re
 import click
 import requests
 
@@ -5,10 +6,19 @@ import requests
 @click.command()
 @click.option('--content', prompt='please type your content', help='This is a string that indicate what you want to classify')
 def response_text(content):
+    click.echo("calculating the result... please wait for seconds")
+
     url = "http://127.0.0.1:8000/predict?content="
     headers = {'accept':'application/json'}
     response = requests.request("POST", url, headers=headers, data=content.encode('utf-8'))
-    click.echo(response.text)
+
+    if "negative" in response.text:
+        # click.echo(response.text)
+        click.echo('This is "NOT" an AD content')
+        click.echo('The probability is {}'.format(re.findall(r"\d+\.\d+", response.text)[0]))
+    else:
+        click.echo('This is an AD content')
+        click.echo('The probability is {}'.format(re.findall(r"\d+\.\d+", response.text)[0]))
 
 if __name__ == '__main__':
     response_text()
