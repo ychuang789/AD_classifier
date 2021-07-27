cf50a9a4c7e12f002e61f61da6a6943b50eebbdc
ychuang <ychuang@eland.com.tw>
2021-07-27 10:01:37 +0800

    modify the output of click app file

--- click_app.py
+++ click_app.py
@@ -7,19 +7,23 @@ from train.predict import single_prediction
 
 hf_logging.set_verbosity_error()
 
+
 @click.command()
-@click.option('--sentence', prompt='your sentence', help='This is a string that indicate what you want to classify')
+@click.option('--sentence', prompt='please type your content', help='This is a string that indicate what you want to classify')
 def prediction(sentence):
-    click.echo('\n')
     click.echo('calculating scores...please wait')
-    click.echo('\n')
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = ADClassifier(len(['0', '1'])).to(device)
-    score, label = single_prediction(model, sentence)
-    if label == 0:
-        click.echo('This content is "NOT" an AD article, the probability is {0}'.format(score))
-    else:
-        click.echo('This content is an AD article, the probability is {0}'.format(score))
+
+    try:
+        score, label = single_prediction(model, sentence)
+        click.echo('The prediction is done...printing the results')
+        if label == 0:
+            click.echo('This content is "NOT" an AD article, the probability is {0}'.format(score))
+        else:
+            click.echo('This content is an AD article, the probability is {0}'.format(score))
+    except:
+        click.echo('There is something run while getting the prediction...')
 
 if __name__ == '__main__':
     prediction()
---
+++ main.py
@@ -0,0 +1,40 @@
+import torch
+import logging
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from transformers import logging as hf_logging
+from typing import Dict
+
+from train.model import ADClassifier
+from train.predict import single_prediction
+
+hf_logging.set_verbosity_error()
+logging.basicConfig()
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = ADClassifier(len(['0', '1'])).to(device)
+
+app = FastAPI(title="AD classifier API", description="A simple API using fine-tuning albert to predict the AD content")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/predict")
+def predict_content(content: str):
+    """
+    A simple function that receive a text content and predict if it is AD article.
+    input param: content
+    return: prediction, probabilities
+    """
+
+    try:
+        score, label = single_prediction(model, content)
+        prediction = {0:'negative', 1:'positive'}
+        result = {'prediction': prediction[label], 'probability': score}
+        return result
+    except:
+        logging.ERROR('There is something wrong while running the app')
+
